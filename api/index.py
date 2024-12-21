62b42c8276784c4fd09d6980418f54244274452c
soonland <76574139+soonland@users.noreply.github.com>
2024-12-21 10:33:14 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        response = requests.get('https://share-a-review.vercel.app/fr')
+        response = requests.get('https://www.youtube.com/watch?v=nN6VR92V70M')
         soup = BeautifulSoup(response.content, 'html.parser')
-        footer = soup.find(attrs={"data-testid": "testid.footer"})
-        footer_text = footer.get_text() if footer else 'Footer not found'
+        title = soup.find('title')
+        title_text = title.get_text() if title else 'Title not found'
 
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(footer_text.encode('utf-8'))
+        self.wfile.write(title_text.encode('utf-8'))
