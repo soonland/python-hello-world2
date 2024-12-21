743f4f3b8f7d1b37b66739005ec1fee8c4c7a072
soonland <76574139+soonland@users.noreply.github.com>
2024-12-21 11:05:00 -0500

    chore: test

--- api/index.py
+++ api/index.py
@@ -6,11 +6,11 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         response = requests.get('https://www.youtube.com/watch?v=nN6VR92V70M')
-        soup = BeautifulSoup(response.content, 'html.parser')
-        title = soup.find('title')
-        title_text = title.get_text() if title else 'Title not found'
+        # soup = BeautifulSoup(response.content, 'html.parser')
+        # title = soup.find('title')
+        # title_text = title.get_text() if title else 'Title not found'
 
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(title_text.encode('utf-8'))
+        self.wfile.write(response.content.encode('utf-8'))
