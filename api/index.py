383db07705c0dbd8e3d82d21a3a632d40e22d720
soonland <76574139+soonland@users.noreply.github.com>
2024-12-21 11:17:19 -0500

    chore: test

--- api/index.py
+++ api/index.py
@@ -5,12 +5,10 @@ from bs4 import BeautifulSoup
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        response = requests.get('https://www.youtube.com/watch?v=nN6VR92V70M')
-        # soup = BeautifulSoup(response.content, 'html.parser')
-        # title = soup.find('title')
-        # title_text = title.get_text() if title else 'Title not found'
+        response = requests.get('https://www.youtube.com/watch?v=7SVBjn4oBBA')
+        response_text = response.text
 
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(response.content.encode('utf-8'))
+        self.wfile.write(response_text.encode('utf-8'))
