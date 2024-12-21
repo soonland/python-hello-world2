b70e775366c9ba08b69a02253f3578684f4a014a
soonland <76574139+soonland@users.noreply.github.com>
2024-12-21 10:21:05 -0500

    chore: test

--- api/index.py
+++ api/index.py
@@ -1,10 +1,16 @@
 from http.server import BaseHTTPRequestHandler
+import requests
+from bs4 import BeautifulSoup
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        response = requests.get('https://share-a-review.vercel.app/fr')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        footer = soup.find(attrs={"data-testid": "testid.footer"})
+        footer_text = footer.get_text() if footer else 'Footer not found'
+
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+        self.wfile.write(footer_text.encode('utf-8'))
